d72ceb8f39093ffa1d71388dcd976a58e84462d1
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2023-02-06 14:48:16 -0500

    Add files via upload

---
+++ bookstore/src/main/java/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+We will use Controllers to map routes to our application.
+The Controller’s objective is to implement the logic behind serving a data to a user.
+*/
+
+@Controller // @Controller will tell Spring to consider the HelloController class as a controller.
+public class HelloController {
+    @RequestMapping("/") // @RequestMapping will map the route '/' to the function helloMessage().
+    // So every time a GET request is send to '/' , the function helloMessage() is called.
+    public String helloMessage(Model model){
+        return "books/hello.html"; // The return value is a template named books/hello.
+        // This string is a reference to the html template that we will build in the next paragraph.
+    }
+
+}
